f70e69c7d3cf8e692eadc42f817bd8e86852262d
juurikkaanritari <alexsonh@ecf.utoronto.ca>
2020-02-16 05:33:30 -0500

    midi trombone ready

--- midi_trombone_input/midi_trombone_input.ino
+++ midi_trombone_input/midi_trombone_input.ino
@@ -1,6 +1,6 @@
 #include <MIDI.h>
 const int potPin = A0; 
-const int  buttonPin0 = 2;
+const int buttonPin0 = 2;
 const int buttonPin1 = 3;
 const int buttonPin2 = 4;
 const int baseOffset = 28;
@@ -17,30 +17,32 @@ int note;
 
  void setup()
  {
-     MIDI.begin();// Listen to all incoming messages
-     Serial.begin(115200);
-     pinMode(buttonPin0, INPUT_PULLUP);
+    MIDI.begin();// Listen to all incoming messages
+    Serial.begin(115200);
+    pinMode(buttonPin0, INPUT_PULLUP);
+    pinMode(buttonPin1, INPUT_PULLUP);
+    pinMode(buttonPin2, INPUT_PULLUP);
  }
 
  void loop()
  {
-   // potValue = analogRead(potPin);
-   buttonValue0 = digitalRead(buttonPin0);
-   Serial.println(buttonValue0);
-   // buttonValue1 = digitalRead(buttonPin1);
-   // buttonValue2 = digitalRead(buttonPin2);
+    potValue = analogRead(potPin);
+    buttonValue0 = digitalRead(buttonPin0);
+    Serial.println(buttonValue0);
+    buttonValue1 = digitalRead(buttonPin1);
+    buttonValue2 = digitalRead(buttonPin2);
 
-   // note = int(potValue * (12.0/1023.0));
+    note = int(potValue * (12.0/1023.0));
 
-   // if (buttonValue0 == 1) {
-   //    note += baseOffset;
-   // } else if (buttonValue1 == 1) {
-   //    note += baseOffset + 12;
-   // } else if (buttonValue2 == 1) {
-   //    note += baseOffset + 24
-   // } else {
-   //    continue;
-   // }
-   // MIDI.sendNoteOn(note, 127, 1);
-   // delay(100); 
+    if (buttonValue0 == 0) {
+       note += baseOffset;
+    } else if (buttonValue1 == 0) {
+       note += baseOffset + 12;
+    } else if (buttonValue2 == 0) {
+       note += baseOffset + 24
+    } else {
+       continue;
+    }
+    MIDI.sendNoteOn(note, 127, 1);
+    delay(100); 
 }
