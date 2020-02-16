3147560efb675bc2dd8ce8828f1f033ccbd6393f
Kelly Wang <kelly.keyi.wang@outlook.com>
2020-02-16 01:10:11 -0500

    Add compare_notes2.py and update mido_input_test

--- mido_input_test.py
+++ mido_input_test.py
@@ -1,26 +1,35 @@
 import mido
-import time 
-
+import time
 print(mido.get_input_names())
 print(mido.get_output_names())
+
+ref = mido.MidiFile('twinkle_twinkle.mid')
 
 mid = mido.MidiFile()
 track = mido.MidiTrack()
 mid.tracks.append(track)
-
 tempo = 500000
 
-with mido.open_input('2- LoopBe Internal MIDI 1') as idevice, mido.open_output('Microsoft GS Wavetable Synth 0') as odevice:
+with mido.open_input('2- LoopBe Internal MIDI 1') as idevice, mido.open_output('VirtualMIDISynth #1 0') as odevice:
+
+    for msg in ref:
+        print(msg)
+        time.sleep(msg.time)
+        if not msg.is_meta:
+            odevice.send(msg)
+
     start = time.time()
     last = 0
     now = 0
+    print("start")
 
-    while now < 10:
+    while now < ref.length*2:
         msg = idevice.poll()
         now = time.time() - start
 
         if msg:
             odevice.send(msg)
+#            msg.time = now - last
             msg.time = int(mido.second2tick(now-last, mid.ticks_per_beat, tempo))
             last = now
             track.append(msg)
