9746423a9e874fd0b8133924a1827f4ff774fff8
Henry <hlewispm2@gmail.com>
2020-02-16 05:26:02 -0500

    Implement trombone + test pullup

--- midi_trombone_input/midi_trombone_input.ino
+++ midi_trombone_input/midi_trombone_input.ino
@@ -1,6 +1,14 @@
 #include <MIDI.h>
-const int analogInPin = A0;  
-int sensorValue = 0;        
+const int potPin = A0; 
+const int  buttonPin0 = 2;
+const int buttonPin1 = 3;
+const int buttonPin2 = 4;
+const int baseOffset = 28;
+
+int potValue = 0;
+int buttonValue0 = 0;
+int buttonValue1 = 0;
+int buttonValue2 =  0;        
 int prevNote = 0;
 int note; 
 
@@ -11,17 +19,28 @@ int note;
  {
      MIDI.begin();// Listen to all incoming messages
      Serial.begin(115200);
+     pinMode(buttonPin0, INPUT_PULLUP);
  }
 
  void loop()
  {
-    sensorValue = analogRead(analogInPin);
-    note = int(sensorValue/85);
-  
-  if (note != prevNote){
-     MIDI.sendNoteOn(40+note, 127, 1);
-     prevNote = note;
-     delay(100); 
-  }
-  
- }
+   // potValue = analogRead(potPin);
+   buttonValue0 = digitalRead(buttonPin0);
+   Serial.println(buttonValue0);
+   // buttonValue1 = digitalRead(buttonPin1);
+   // buttonValue2 = digitalRead(buttonPin2);
+
+   // note = int(potValue * (12.0/1023.0));
+
+   // if (buttonValue0 == 1) {
+   //    note += baseOffset;
+   // } else if (buttonValue1 == 1) {
+   //    note += baseOffset + 12;
+   // } else if (buttonValue2 == 1) {
+   //    note += baseOffset + 24
+   // } else {
+   //    continue;
+   // }
+   // MIDI.sendNoteOn(note, 127, 1);
+   // delay(100); 
+}
